e7b05f139eee22375fac216d98ddb6dea055b3d0
Igor Moura <igor.m.moura@group.issworld.com>
2026-01-23 09:07:30 +0000

    fix: add JsonPropertyName attributes to PlaceOrderDto and ProductDto properties

--- src/WebApp/Dto/PlaceOrderDto.cs
+++ src/WebApp/Dto/PlaceOrderDto.cs
@@ -1,18 +1,34 @@
+using System.Text.Json.Serialization;
+
 namespace WebApp.Dto;
 
 public class PlaceOrderDto
 {
+    [JsonPropertyName("orderId")]
     public string OrderId { get; set; } = string.Empty;
+    
+    [JsonPropertyName("time")]
     public DateTime Time { get; set; }
+    
+    [JsonPropertyName("products")]
     public List<ProductDto> Products { get; set; } = new();
+    
+    [JsonPropertyName("value")]
     public decimal Value { get; set; }
 }
 
 public class ProductDto
 {
+    [JsonPropertyName("productId")]
     public string ProductId { get; set; } = string.Empty;
+    
+    [JsonPropertyName("name")]
     public string Name { get; set; } = string.Empty;
+    
+    [JsonPropertyName("value")]
     public decimal Value { get; set; }
+    
+    [JsonPropertyName("amount")]
     public int Amount { get; set; }
 }
 
